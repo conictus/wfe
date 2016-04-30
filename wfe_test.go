532a80eae69279f4a2d44b80b487102aadbf4e1d
Muhamad Azmy <azmy@conictus.com>
2016-04-30 16:51:25 +0200

    more wfe tests

--- wfe_test.go
+++ wfe_test.go
@@ -3,6 +3,7 @@ package wfe
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
@@ -24,6 +25,10 @@ func wfeTestPtr(c *Context, a *wfeTestStruct) string {
 
 func wfeTestInter(c *Context, a fmt.Stringer) string {
 	return a.String()
+}
+
+func wfeTestPanic(c *Context) {
+	panic("i paniced")
 }
 
 func TestHandleRequestUnknownFn(t *testing.T) {
@@ -90,6 +95,129 @@ func TestHandleRequestInterOk(t *testing.T) {
 	}
 
 	if ok := assert.Equal(t, x.s, v); !ok {
+		t.Fatal()
+	}
+}
+
+type testDelivery struct {
+	val requestImpl
+	mock.Mock
+}
+
+func (d *testDelivery) ID() string {
+	args := d.Called()
+	return args.String(0)
+}
+
+func (d *testDelivery) Confirm() error {
+	args := d.Called()
+	return args.Error(0)
+}
+
+func (d *testDelivery) Content(c interface{}) error {
+	val := c.(*requestImpl)
+	*val = d.val
+	//val.Function = "github.com/conictus/wfe.wfeAddTest"
+	//val.Arguments = []interface{}{1, 2}
+	return nil
+}
+
+func TestHandleDeliverOk(t *testing.T) {
+	store := &testStore{}
+	eng := &Engine{store: store}
+
+	d := testDelivery{val: requestImpl{
+		Function:  "github.com/conictus/wfe.wfeAddTest",
+		Arguments: []interface{}{1, 2},
+	}}
+
+	d.On("ID").Return("1234")
+	d.On("Confirm").Return(nil)
+
+	store.On("Set", &Response{
+		UUID:   "1234",
+		State:  StateSuccess,
+		Result: 3,
+	}).Return(nil)
+
+	err := eng.handleDelivery(&d)
+
+	if ok := assert.Nil(t, err); !ok {
+		t.Fatal()
+	}
+
+	if ok := d.AssertExpectations(t); !ok {
+		t.Fatal()
+	}
+
+	if ok := store.AssertExpectations(t); !ok {
+		t.Fatal()
+	}
+}
+
+func TestHandleDeliverHandleErr(t *testing.T) {
+	store := &testStore{}
+	eng := &Engine{store: store}
+
+	d := testDelivery{val: requestImpl{
+		Function:  "github.com/conictus/wfe.unknowFunctions",
+		Arguments: []interface{}{1, 2},
+	}}
+
+	d.On("ID").Return("1234")
+	d.On("Confirm").Return(nil)
+
+	store.On("Set", &Response{
+		UUID:  "1234",
+		State: StateError,
+		Error: ErrUnknownFunction.Error(),
+	}).Return(nil)
+
+	err := eng.handleDelivery(&d)
+
+	if ok := assert.Error(t, err); !ok {
+		t.Fatal()
+	}
+
+	if ok := d.AssertExpectations(t); !ok {
+		t.Fatal()
+	}
+
+	if ok := store.AssertExpectations(t); !ok {
+		t.Fatal()
+	}
+}
+
+func TestHandleDeliverHandleFnPanic(t *testing.T) {
+	Register(wfeTestPanic)
+	store := &testStore{}
+	eng := &Engine{store: store}
+
+	d := testDelivery{val: requestImpl{
+		Function:  "github.com/conictus/wfe.wfeTestPanic",
+		Arguments: []interface{}{},
+	}}
+
+	d.On("ID").Return("1234")
+	d.On("Confirm").Return(nil)
+
+	store.On("Set", &Response{
+		UUID:  "1234",
+		State: StateError,
+		Error: "i paniced",
+	}).Return(nil)
+
+	err := eng.handleDelivery(&d)
+
+	if ok := assert.Nil(t, err); !ok {
+		t.Fatal()
+	}
+
+	if ok := d.AssertExpectations(t); !ok {
+		t.Fatal()
+	}
+
+	if ok := store.AssertExpectations(t); !ok {
 		t.Fatal()
 	}
 }
